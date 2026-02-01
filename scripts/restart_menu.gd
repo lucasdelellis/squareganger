6d5166995acfed04388cf15af2a0ec3ce7090948
Lucas de Lellis <lucasdelellis2002@gmail.com>
2026-02-01 16:33:18 +0000

    fix: mask selector in restart screen.

--- scripts/restart_menu.gd
+++ scripts/restart_menu.gd
@@ -2,54 +2,45 @@ extends Node
 
 var MAIN_SCENE = "res://scenes/game_manager.tscn"
 const MASK_PATH = 'res://assets/masks/m'
-#var player_masks: Array[int] = GameState.masks
-#var selected_masks : Array[int] = [1,2] 
+
+var left_mask = -1
+var right_mask = -1
 
 func _ready() -> void:
-	#set_masks()
 	var masks = []
-	for mask_id in [1,2,3,4,5]:
+	for mask_id in GameState.obtained_masks:
 		masks.append({"maskId": mask_id, "imagePath": get_mask_img(mask_id)})
 	$UnlockedMaskSelector.render_masks(masks)
 	$UnlockedMaskSelector.mask_selected.connect(_on_mask_selected)
+	$UnlockedMaskSelector.mask_deselected.connect(_on_mask_deselected)
 	$Score.text = str(GameState.score)
-
-#func set_masks():
-	#var masks = []
-	#print(player_masks)
-	#for mask_id in player_masks:
-		#masks.append({"maskId": mask_id, "imagePath": get_mask_img(mask_id)})
-	#print(masks)	
-	#$MaskSelector.render_masks(masks)
-	#$MaskSelector.mask_selected.connect(_on_mask_selected)
 
 func reset_game_state():
 	GameState.score = 0
-	#GameState.masks = selected_masks
-	#GameState.obtained_masks = selected_masks
+	GameState.masks = [left_mask, right_mask]
+	GameState.obtained_masks = [left_mask, right_mask]
 
 func _on_restart_button_pressed() -> void:
-	reset_game_state()
-	get_tree().change_scene_to_file(MAIN_SCENE)
-	
-#func _on_mask_selected(id: int): #it may work
-	#print(id)
-	#var mask_path = get_mask_img(id)
-	##Mask selection
-	#if GameState.masks.has(id):
-		#GameState.masks.erase(id)
-		#$SelectedMasks.eliminate_mask(id)
-	#else:
-		#if GameState.masks.size()<2:
-			#GameState.masks.append(id)
-			#$SelectedMasks.set_mask(id)
+	if left_mask != -1 and right_mask != -1:
+		reset_game_state()
+		get_tree().change_scene_to_file(MAIN_SCENE)
 	
 func get_mask_img(n: int):
 	return MASK_PATH + str(n) + '.png'
 
-func _on_mask_selected():
-	var selected_masks = $UnlockedMaskSelector.selected_masks
-	if len(selected_masks) >= 1:
-		$LeftMask.texture = load(get_mask_img($UnlockedMaskSelector.selected_masks[0]))
-	if len(selected_masks) >= 2:
-		$RightMask.texture = load(get_mask_img($UnlockedMaskSelector.selected_masks[1]))
+func _on_mask_selected(id: int):
+	var texture = load(get_mask_img(id))
+	if left_mask == -1:
+		$LeftMask.texture = texture
+		left_mask = id
+	else:
+		$RightMask.texture = texture
+		right_mask = id
+		
+func _on_mask_deselected(id: int):
+	if left_mask == id:
+		$LeftMask.texture = null
+		left_mask = -1
+	elif right_mask == id:
+		$RightMask.texture = null
+		right_mask = -1
